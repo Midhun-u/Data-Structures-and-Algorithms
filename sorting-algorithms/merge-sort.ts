3dd0eded4f9dc1b6e7c2bcc9c39e61449e63414c
Midhun U <midhunvkm689@gmail.com>
2026-05-26 10:34:54 +0530

    Working on merge sort

---
+++ sorting-algorithms/merge-sort.ts
@@ -0,0 +1,11 @@
+function mergeSort(arr: Array<number>){
+
+    const newArr = [...arr]
+
+    if(newArr.length <= 1){
+        return newArr
+    }
+
+    const middle = Math.floor(newArr.length / 2)
+
+}
