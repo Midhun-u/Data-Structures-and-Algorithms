a0020e949c9175b27f64f5251201da780d843d74
Midhun U <midhunvkm689@gmail.com>
2026-05-26 11:11:13 +0530

    Completed merge sort

--- sorting-algorithms/merge-sort.ts
+++ sorting-algorithms/merge-sort.ts
@@ -1,4 +1,29 @@
-function mergeSort(arr: Array<number>){
+function merge(leftArr: Array<number>, rightArr: Array<number>): Array<number>{
+
+    const sortedArr: Array<number> = []
+    let i = 0
+    let j = 0
+
+    while(i < leftArr.length && j < rightArr.length){
+        if(leftArr[i] < rightArr[j]){
+            sortedArr.push(leftArr[i])
+            i++
+        }else{
+            sortedArr.push(rightArr[j])
+            j++
+        }
+    }
+
+    return [
+        ...sortedArr,
+        ...leftArr.slice(i),
+        ...rightArr.slice(j)
+    ]
+
+}
+
+
+function mergeSort(arr: Array<number>): Array<number>{
 
     const newArr = [...arr]
 
@@ -8,4 +33,22 @@ function mergeSort(arr: Array<number>){
 
     const middle = Math.floor(newArr.length / 2)
 
+    const left = newArr.slice(0, middle)
+    const right = newArr.slice(middle)
+
+    return merge(
+        mergeSort(left),
+        mergeSort(right)
+    )
+
 }
+
+const sortedArr = mergeSort([5, 2, 1, 3, 4])
+console.log(sortedArr)
+
+/*
+    [5, 2, 1, 3, 4] -> [1, 2, 3, 4, 5]
+    Time Complexity = o(n*logn)
+*/
+
+export {}
